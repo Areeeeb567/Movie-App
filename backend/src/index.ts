02d681b4cc64c04fd8cf03b87f1be4a6ae6c5fcc
Areeb <areebfaisal49@gmail.com>
2025-07-10 01:09:21 +0500

    Separated MongoDB connection and added PORT in .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
-
+import connectDB from './config/db';
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,11 +19,17 @@ app.get('/', (req, res) => {
     res.send('API is working!');
 });
 
-// ✅ Only ONE app.listen call
-mongoose.connect('mongodb://localhost:27017/auth-demo')
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch(err => {
-        console.error("MongoDB connection error:", err);
-    });
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+//
+// mongoose.connect('mongodb://localhost:27017/auth-demo')
+//     .then(() => {
+//         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//     })
+//     .catch(err => {
+//         console.error("MongoDB connection error:", err);
+//     });
