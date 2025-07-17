767b41b9c23b587878708af36b0818b3cf974b38
Areeb <areebfaisal49@gmail.com>
2025-07-17 10:44:11 +0500

    Removed Extra API Functions

--- backend/src/routes/movie.ts
+++ backend/src/routes/movie.ts
@@ -1,15 +1,7 @@
 import express from 'express';
-import {getMovieDetails, getPopularMovies, searchMovies} from '../controllers/tmdb';
+import { getPopularMovies } from '../controllers/tmdb';
 
 const router = express.Router();
-
-/**
- * Route to search for movies
- * @route GET /api/movies/search
- * @queryParam {string} query - The search query for movies
- * @queryParam {number} [page=1] - The page number for pagination
- */
-router.get('/search', searchMovies);
 
 /**
  * Route to get popular movies
@@ -17,12 +9,5 @@ router.get('/search', searchMovies);
  * @queryParam {number} [page=1] - The page number for pagination
  */
 router.get('/popular', getPopularMovies);
-
-/**
- * Route to get movie details by ID
- * @route GET /api/movies/:movieId
- * @param {string} movieId - The ID of the movie to fetch details for
- */
-router.get('/:movieId', getMovieDetails);
 
 export default router;
