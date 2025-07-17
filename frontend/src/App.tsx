cc1374d4ad1a016865375254e6ba5a2020b0cd7d
Areeb <areebfaisal49@gmail.com>
2025-07-17 22:00:58 +0500

    Added Material UI and the theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import Register from './pages/Register';
+// import Page from './templates/page';
 
 // Main App component that defines the routes for the application
 const App = () => {
     return (
         <Routes>
             {/*Define the routes for the application, including public and protected routes*/}
+            {/*<Route path="/" element={<Page />} />*/}
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
---
+++ frontend/src/assets/theming/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#0a0a0f',
+            paper: '#1c0b2e',
+        },
+        text: {
+            primary: '#ffffff',
+        },
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    height: '100vh',
+                    margin: 0,
+                    background: 'linear-gradient(135deg, #0a0a0f 0%, #1c0b2e 50%, #2a0e4f 100%)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#fff',
+                },
+            },
+        },
+    },
+});
+
+export default theme;
