88419cb248e189c49cb7bed045322fc06707e8f3
Areeb <areebfaisal49@gmail.com>
2025-07-17 10:46:19 +0500

    Removed Extra API Functions

--- backend/src/controllers/tmdb.ts
+++ backend/src/controllers/tmdb.ts
@@ -7,59 +7,6 @@ import {ERROR_MESSAGES, HTTP_STATUS, jsonResponse, sendErrorResponse} from "../c
  */
 
 /**
- * Fetch movie details from TMDB API
- * @param req
- * @param res
- * @return Json response with movie details or error message
- */
-export const getMovieDetails = async (req: Request, res: Response): Promise<void> => {
-    const { movieId } = req.params;
-
-    try {
-        const response = await axios.get(`${process.env.TMDB_BASE_URL}/movie/${movieId}`, {
-            params: {
-                api_key: process.env.TMDB_API_KEY,
-                language: 'en-US'
-            }
-        });
-        jsonResponse(res, response.data);
-    }
-    catch (error) {
-        sendErrorResponse(res, HTTP_STATUS.SERVER_ERROR, ERROR_MESSAGES.ERROR_FETCHING_MOVIES, false);
-    }
-};
-
-/**
- * Search for movies using TMDB API
- * @param req
- * @param res
- * @return Json response with search results or error message
- */
-export const searchMovies = async (req: Request, res: Response): Promise<void> => {
-    const {query, page = 1} = req.query;
-
-    // In case of no query
-    if (!query) {
-        sendErrorResponse(res, HTTP_STATUS.BAD_REQUEST, 'Query parameter is required and must be a string');
-        return;
-    }
-
-    try {
-        const response = await axios.get(`${process.env.TMDB_BASE_URL}/search/movie`, {
-            params: {
-                api_key: process.env.TMDB_API_KEY,
-                query: query,
-                page: page,
-            },
-        });
-
-        jsonResponse(res, response.data);
-    } catch (error) {
-        sendErrorResponse(res, HTTP_STATUS.SERVER_ERROR, ERROR_MESSAGES.ERROR_FETCHING_MOVIES, false);
-    }
-};
-
-/**
  * Fetch movie genres from TMDB API
  * @param req
  * @param res
--- backend/src/routes/movie.ts
+++ backend/src/routes/movie.ts
@@ -1,27 +1,12 @@
 import express from 'express';
-import {getGenres, getMovieDetails, searchMovies} from '../controllers/tmdb';
+import {getGenres} from '../controllers/tmdb';
 
 const router = express.Router();
-
-/**
- * Route to search for movies
- * @route GET /api/movies/search
- * @queryParam {string} query - The search query for movies
- * @queryParam {number} [page=1] - The page number for pagination
- */
-router.get('/search', searchMovies);
 
 /**
  * Route to get movie genres
  * @route GET /api/movies/genres
  */
 router.get('/genres', getGenres);
-
-/**
- * Route to get movie details by ID
- * @route GET /api/movies/:movieId
- * @param {string} movieId - The ID of the movie to fetch details for
- */
-router.get('/:movieId', getMovieDetails);
 
 export default router;
